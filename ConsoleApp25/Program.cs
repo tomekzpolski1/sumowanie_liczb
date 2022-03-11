dcf228fe41462b78f4b04520d57026690f45b9e0
tomekzpolski1 <tomasz.rozynski92@gmail.com>
2022-03-11 19:06:53 +0100

    Add project files.

---
+++ ConsoleApp25/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ConsoleApp25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczby:");
+
+            int wynik = 0;
+            do
+            {
+                int liczba = int.Parse(Console.ReadLine());
+                if (liczba == 0) //exit
+                    break;
+                else
+                { wynik += liczba; } //sum
+
+            } while (true);
+            Console.WriteLine("Suma wprowadzonych liczb to {0}", wynik);
+        }
+    }
+}
